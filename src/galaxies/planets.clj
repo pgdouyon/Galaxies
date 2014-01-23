811e0198fa23881b767c5c1eac731158817f07a7
pgdouyon <pgdouyon@alum.mit.edu>
2014-01-23 03:18:57 -0500

    add mines and fields - Science & Engineering
    Add support for building mines and schools for the fields of Science and
    Engineering.  Reorganized some of the update functions and abstracted
    them out into one general case.

--- src/galaxies/planets.clj
+++ src/galaxies/planets.clj
@@ -37,20 +37,74 @@
   [name]
   {:name name
    :pop 0
+   :widgets 0
+   :mines {:Technium 0
+           :Scienine 0}
+   :elements {:Technium 0
+              :Scientine 0}
+   :schools {:Engineering 0
+             :Physics 0}
+   :workers {:Engineers 0
+             :Physicists 0}
    :xcoord (gen-coord)
    :ycoord (gen-coord)})
 
-(defn- planet-coords [p]
+(defn- homebase [p pop widgets]
+  (assoc p
+    :pop pop
+    :widgets widgets))
+
+(defn- widget-cost [amount]
+  (* amount 100))
+
+(defn- element-cost [amount]
+  (* amount 1000))
+
+#_(defn- planet-coords [p]
   (str (:xcoord p) "," (:ycoord p)))
 
 (defn- planet-id [p]
-  (keyword (planet-coords p)))
+  (keyword (:name p)))
 
-(defn- alter-pop [p delta]
-  (update-in p [:pop] + delta))
+
+;===========================================================================
+;Update Methods
+;===========================================================================
+(defn- alter-attrib [p delta keys]
+  (update-in p keys + delta))
+
+(defmacro build-in 
+  "Anaphoric macro that captures the 'p' and 'amount' arguments from the calling function.
+Used to build new objects by updating the appropriate attributes in the planet object (charging the source
+and growing the target)."
+  [target source cost-func]
+  `(let [cost# (- (#'~cost-func ~'amount))]
+     (-> ~'p
+       (alter-attrib ~'amount ~target)
+       (alter-attrib cost# ~source))))
+
+(defn- build-mines [p amount field]
+  (build-in [:mines field] [:widgets] widget-cost))
+
+(defn- build-schools [p amount field]
+  (build-in [:schools field] [:elements field] element-cost))
+
+
+;===========================================================================
+;Simulation Methods
+;===========================================================================
+(defn- grow-attrib [p rate keys]
+  (update-in p keys * rate))
 
 (defn- grow-pop [p]
-  (update-in p [:pop] * growth-rate))
+  (grow-attrib p growth-rate [:pop]))
+
+;===========================================================================
+;API Methods
+;===========================================================================
+(defn simulate-year []
+  (swap! planet-map (partial map grow-pop)))
+
 
 ;===========================================================================
 ;Initialization
@@ -68,12 +122,10 @@ A random number of names is returned varying from [min-planets, min-planets + va
   (let [planets (map planet names)]
     (into (hash-map)
           (map (juxt planet-id identity) planets))))
-  
+
 (defn init! []
   (let [names (planet-names!)
         planets (new-planet-map names)]
     (update-planets! planets)))
 
 
-(defn simulate-year []
-  (swap! planet-map (partial map grow-pop)))
